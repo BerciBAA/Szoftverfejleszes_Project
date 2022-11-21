9211fd9a819e0a0ad0b79fd233b18b69f951f294
vitkosbence <vitkosbence@gmail.com>
2022-11-21 02:48:08 +0100

    profile editing added

--- profile.php
+++ profile.php
@@ -1,13 +1,70 @@
 <?php
 session_start();
   
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === false){
-    header("location: logout.php");
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === false){
+    header("location: login.php");
     exit;
 }
  
 require_once "config.php";
 
-include "templates/profile.html";
+$params = [];
+$params["action"] = isset($_POST["action"]) ? $_POST["action"] : "";
+if(isset($_POST["form"])){
+    $params["username"] = "";
+    $params["birthdate"] = "1000-01-01";
+    $params["microphone"] = 0;
+    $params["gender"] = 0;
+    $params["description"] = "";
+    foreach($_POST["form"] as $input){
+        if($input["name"] == "microphone"){
+            if($input["value"] == "on"){
+                $params["microphone"] = 1;
+            }
+            else{
+                $params["microphone"] = 2;
+            }
+        }
+        else{
+            $params[$input["name"]] = $input["value"];
+        }
+    }
+}
+
+
+if($params["action"] == "show"){
+    $sql = "SELECT * FROM users WHERE username = ?";
+        
+    if($stmt = mysqli_prepare($link, $sql)){
+        $stmt->bind_param("s", $param_username);
+        
+        $param_username = $_SESSION["username"];
+        
+        if(mysqli_stmt_execute($stmt)){            
+            echo(json_encode(mysqli_fetch_assoc(mysqli_stmt_get_result($stmt))));
+        } else{
+            $errors[] = "db_error";
+        }
+        mysqli_stmt_close($stmt);
+    }
+}
+elseif($params["action"] == "set"){ 
+    $sql = "UPDATE `users` SET `username` = ?, `birthdate` = ?, `microphone` = ?, `gender` = ?, `description` = ?  WHERE username = ?";
+        
+    if($stmt = mysqli_prepare($link, $sql)){
+        $stmt->bind_param("ssiiss", $params["username"], $params["birthdate"], $params["microphone"], $params["gender"], $params["description"], $param_username);
+
+        $param_username = $_SESSION["username"];
+        
+        if(!mysqli_stmt_execute($stmt)){
+            $errors[] = "db_error";
+        }
+        mysqli_stmt_close($stmt);
+    }
+}
+else{
+    include "templates/profile.html";
+}
+
 
 ?>
